9a3f041310af657ccacd9d1f1cb2801eb1b079ad
Sergio Medina <63879459+sergiomedinaio@users.noreply.github.com>
2022-10-20 12:11:02 -0300

    refactor: update date definitions

--- src/Main.java
+++ src/Main.java
@@ -3,15 +3,8 @@ import java.util.Date;
 public class Main {
     public static void main(String args[]) {
         System.out.println("Da Vinci Tournament");
-        Date comienzoDeCursada = new Date();
-        comienzoDeCursada.setYear(2022);
-        comienzoDeCursada.setMonth(7); //Enero 0, Agosto 7
-        comienzoDeCursada.setDate(18);
-
-        Date finDeCursada = new Date();
-        finDeCursada.setYear(2022);
-        finDeCursada.setMonth(11); //Enero 0, Dic 11
-        finDeCursada.setDate(5);
+        Date comienzoDeCursada = new Date(2022, 7, 18);
+        Date finDeCursada = new Date(2022, 11, 5);
 
         Season torneoDaVinci = new Season(
             "Torneo 2022, 2do cuatri",
